83f7d266652fb8ab396afbfa9880bfe75aff4d14
vicente-priego-aviles <vicente.priego.aviles@gmail.com>
2025-10-17 12:00:17 +0200

    🎉 Initial commit: MVP v1.0.0

---
+++ src/main/java/dev/javacadabra/reservasviaje/ReservasviajeApplication.java
@@ -0,0 +1,13 @@
+package dev.javacadabra.reservasviaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasviajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasviajeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/javacadabra/reservasviaje/ReservasviajeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.javacadabra.reservasviaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasviajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
